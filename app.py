ad35a4fd8f6028d72dc99f6c35831412766461bc
mahlvm <mahmoreti@hotmail.com>
2023-12-06 15:50:51 +0000

    exercise 03

--- app.py
+++ app.py
@@ -5,18 +5,8 @@ from lib.album_repository import AlbumRepository
 from lib.artist_repository import ArtistRepository
 
 
-# Create a new Flask app
+
 app = Flask(__name__)
-
-# @app.route('/emoji', methods=['GET'])
-# def get_emoji():
-#     return render_template('emoji.html', emoji=':)')
-# from example_routes import apply_example_routes
-# apply_example_routes(app)
-
-# @app.route('/goodbye')
-# def get_goodbye():
-#     return render_template("goodbye.html")
 
 @app.route('/albums')
 def get_albums():
@@ -25,12 +15,13 @@ def get_albums():
     albums = repository.all()
     return render_template ("albums/index.html", albums=albums)
 
-# @app.route('/artists')
-# def get_artist():
-#     connection = get_flask_database_connection(app)
-#     repository = ArtistRepository(connection)
-#     artists = repository.all()
-#     return render_template ("artists/index.html", artists=artists)
+@app.route('/albums/<album_id>')
+def get_album(album_id):
+    connection = get_flask_database_connection(app)
+    repository_album = AlbumRepository(connection)
+    album = repository_album.find(album_id)
+    return render_template ("albums/show.html", album=album)
+
 
 @app.route('/artists/<album_id>')
 def get_album_find(album_id):
@@ -41,8 +32,18 @@ def get_album_find(album_id):
     artists = repository_artist.find(album_id)
     return render_template ("artists/index.html", artists=artists, albums=albums)
 
+# @app.route('/artists')
+# def get_artist():
+#     connection = get_flask_database_connection(app)
+#     repository = ArtistRepository(connection)
+#     artists = repository.all()
+#     return render_template ("artists/index.html", artists=artists)
+
+
 
 
 
 if __name__ == '__main__':
     app.run(debug=True, port=int(os.environ.get('PORT', 5001)))
+
+
--- tests/test_app.py
+++ tests/test_app.py
@@ -3,20 +3,11 @@ from playwright.sync_api import Page, expect
 def test_get_albums(page, test_web_address, db_connection):
     db_connection.seed("seeds/record_store.sql")
     page.goto (f"http://{test_web_address}/albums")
-    div_tags = page.locator("div")
-    expect (div_tags).to_have_text ([
-        "Title: Doolittle\nReleased: 1989",
-        "Title: Surfer Rosa\nReleased: 1988"
-])
-
-# def test_get_artists(page, test_web_address, db_connection):
-#     db_connection.seed("seeds/record_store.sql")
-#     page.goto (f"http://{test_web_address}/artists")
-#     div_tags = page.locator("h1")
-#     expect (div_tags).to_have_text ([
-#         "Title: Doolittle\nReleased: 1989",
-#         "Title: Surfer Rosa\nReleased: 1988"
-# ])
+    li_tags = page.locator("li")
+    expect (li_tags).to_have_text ([
+        "Doolittle",
+        "Surfer Rosa",
+    ])
 
 
 def test_get_albums_just_one(page, test_web_address, db_connection):
@@ -33,35 +24,23 @@ def test_get_albums_just_one(page, test_web_address, db_connection):
     ])
     
 
+def test_visit_album_show_page(page, test_web_address, db_connection):
+    db_connection.seed("seeds/record_store.sql")
+    page.goto (f"http://{test_web_address}/albums")
+    page.wait_for_load_state("load")
+    page.click("text=Surfer Rosa")
+    h1_tag = page.locator("h1")
+    expect (h1_tag).to_have_text("Album: Surfer Rosa")
+    release_year_tag = page.locator(".t-release-year")
+    expect(release_year_tag).to_have_text("Released: 1988")
 
 
 
+def test_visit_album_show_page_and_go_back(page, test_web_address, db_connection):
+    db_connection.seed("seeds/record_store.sql")
+    page.goto (f"http://{test_web_address}/albums")
+    page.click("text=Surfer Rosa")
+    page.click("text=Go back to the album list")
+    h1_tag = page.locator("h1")
+    expect (h1_tag).to_have_text("Albums")
 
-# # Tests for your routes go here
-
-# # === Example Code Below ===
-
-# """
-# We can get an emoji from the /emoji page
-# """
-# def test_get_emoji(page, test_web_address): # Note new parameters
-#     # We load a virtual browser and navigate to the /emoji page
-#     page.goto(f"http://{test_web_address}/emoji")
-
-#     # We look at the <strong> tag
-#     strong_tag = page.locator("strong")
-
-#     # We assert that it has the text ":)"
-#     expect(strong_tag).to_have_text(":)")
-
-# # === End Example Code ===
-
-# # def test_get_hello(page, test_web_adress):
-# #     page.goto(f"http://{test_web_adress}/hello")
-# #     heading_tag = page.locator("h1")
-# #     expect(heading_tag).to_have_text("Hello, world")
-
-# def test_get_goodbye(page, test_web_address):
-#     page.goto(f"http://{test_web_address}/goodbye")
-#     strong_tag = page.locator("strong")
-#     expect(strong_tag).to_have_text("Bye!")
